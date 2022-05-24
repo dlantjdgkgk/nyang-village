b4031bd1027f1f5081d6181b68265009a12ac1ff
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-05-24 23:29:37 +0900

    Main page

---
+++ src/Main/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Body } from './style';
+const Main = () => {
+    return (
+        <>
+            <Body>안녕하세요</Body>
+        </>
+    );
+};
+
+export default Main;
---
+++ src/Main/style.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Body = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    > .wireframe {
+        max-width: 1200px;
+        width: 100%;
+        height: 100%;
+        background-color: red;
+    }
+`;
