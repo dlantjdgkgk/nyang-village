432e7d2e04a68e089b276c06affc0329df1ce772
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-05-30 22:36:59 +0900

    loginPage

---
+++ src/component/Main/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Body } from './style';
+const Main = () => {
+    return (
+        <>
+            <Body>
+                <div className='wireframe'>
+                    <img src='./img/Home.png'></img>
+                    <span className='projectName'>#NYANG VILLAGE</span>
+                </div>
+            </Body>
+        </>
+    );
+};
+
+export default Main;
---
+++ src/component/Main/style.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Body = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    > .wireframe {
+        max-width: 800px;
+        width: 100%;
+        background: linear-gradient(to right, #ffb910, #ff8400);
+    }
+`;
